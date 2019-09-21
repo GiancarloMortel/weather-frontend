ed96bc36b5d1dcc405a4ccaa23a2a6f6de4ed3f6
morteg1@unlv.nevada.edu <morteg1@unlv.nevada.edu>
2019-09-20 21:17:49 -0700

    changed color scheme

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -155,8 +155,7 @@ const Weather = (props) => {
   // when cityid changed get weather
   useEffect(() => {
     const getWeatherApi = () => {
-      // const url = `http://ec2-3-91-181-248.compute-1.amazonaws.com/weather/${props.cityId}`;
-      const url = `http://localhost:3000/weather/${props.cityId}`;
+      const url = `http://ec2-34-227-116-199.compute-1.amazonaws.com/weather/${props.cityId}`;
       const promise = axios.get(url);
   
       promise
